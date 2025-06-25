07996a152c6fcc748f4490600fea83967e3a35d3
Tsinswreng <Tsinswreng@gmail.com>
2025-06-25 15:26:52 +0800

    Cfgˋ獨立

--- Infra/Cfg/ICfgAccessor.cs
+++
@@ -1,9 +0,0 @@
-namespace Ngaq.Core.Infra.Cfg;
-
-public interface ICfgAccessor{
-	public ICfgValue? GetByPath(IList<str> Path);
-	public nil SetByPath(IList<str> Path, ICfgValue Value);
-	public nil RmPath(IList<str> Path);
-}
-
-
