51647dc667640e54741c8fca05af91b447ac7afa
Tsinswreng <Tsinswreng@gmail.com>
2025-06-26 11:01:18 +0800

    文件更名

---
+++ Infra/Errors/ITypedStatus.cs
@@ -0,0 +1,12 @@
+namespace Ngaq.Core.Infra.Errors;
+
+public interface ITypedStatus{
+	/// <summary>
+	/// 狀態標識 作錯誤碼等
+	/// </summary>
+	public object Status { get;set; }
+	/// <summary>
+	/// 類型/命名空間。(Status, Type)當唯一
+	/// </summary>
+	public str? StatusType { get;set; }
+}
