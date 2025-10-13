0d0f144d5388009734379e80cb8b3ab791f1c317
Tsinswreng <Tsinswreng@gmail.com>
2025-10-13 20:15:11 +0800

    遷ConstApiUrl

--- Infra/Url/ConstApiUrl.cs
+++ Infra/Url/ConstApiUrl.cs
@@ -26,19 +26,19 @@ public record struct Url(str V){
 
 public partial class ConstUrl{
 
-	public static str V1 = Mk(null, ["V1"]);
-		public static str Sys = Mk(V1, ["Sys"]);
-			public static str User = Mk(Sys, ["User"]);
+	public static Url V1 = Mk(null, ["V1"]);
+		public static Url Sys = Mk(V1, ["Sys"]);
+			public static Url User = Mk(Sys, ["User"]);
 			//~User
 		//~Sys
 	//~V1
 
 	public partial class UrlUser{
-		//public static str _Root = "";
-		public static str _Root = ConstUrl.User;
-		public static str Login = Mk(_Root, ["Login"]);
-		public static str Logout = Mk(_Root, ["Logout"]);
-		public static str AddUser = Mk(_Root, ["AddUser"]);
+		//public static Url _Root = "";
+		public static Url _Root = ConstUrl.User;
+		public static Url Login = Mk(_Root, ["Login"]);
+		public static Url Logout = Mk(_Root, ["Logout"]);
+		public static Url AddUser = Mk(_Root, ["AddUser"]);
 	}
 
 }
