08d30e995db3140fb436b249d33616c35908af4f
Tsinswreng <Tsinswreng@gmail.com>
2025-10-25 12:47:28 +0800

    IFrontendUserCtx 區分LocalId與RemoteId

--- Frontend/User/IFrontendUserCtx.cs
+++ Frontend/User/IFrontendUserCtx.cs
@@ -1,7 +1,14 @@
 namespace Ngaq.Core.Frontend.User;
+
+using Ngaq.Core.Shared.User.Models.Po.User;
 using Ngaq.Core.Shared.User.UserCtx;
 
+/// <summary>
+/// do not use UserId in IFrontendUserCtx. Use LocalUserId or RemoteUserId
+/// </summary>
 public interface IFrontendUserCtx: IUserCtx{
 	public str? AccessToken{get;set;}
 	public str? RefreshToken{get;set;}
+	public IdUser LocalUserId{get;set;}
+	public IdUser RemoteUserId{get;set;}
 }
