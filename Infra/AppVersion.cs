7036eeba7b8ef56610711896a181442b18fc08bc
Tsinswreng <Tsinswreng@gmail.com>
2025-11-15 21:38:38 +0800

    發版

--- Infra/AppVersion.cs
+++ Infra/AppVersion.cs
@@ -3,7 +3,7 @@ namespace Ngaq.Core.Infra;
 public class AppVer{
 	protected static AppVer? _Inst = null;
 	public static AppVer Inst => _Inst??= new AppVer();
-	//2025-11-14T22:55:37.127+08:00_W46-5
-	//2025_1114_225537
-	public Version Ver { get; set; } = new (1, 0, 4, 254652255);
+	//2025-11-15T21:36:57.096+08:00_W46-6
+	//2025_1115_213659
+	public Version Ver { get; set; } = new (1, 0, 4, 254662136);
 }
