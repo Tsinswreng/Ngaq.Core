207d120075e5341eb0a771b32d19aa70c23a00cc
Tsinswreng <Tsinswreng@gmail.com>
2026-02-27 22:03:38 +0800

    I_RegisterGlobalHotKeys亦用異常包裝

--- Frontend/Hotkey/I_RegisterGlobalHotKeys.cs
+++ Frontend/Hotkey/I_RegisterGlobalHotKeys.cs
@@ -1,8 +1,9 @@
 namespace Ngaq.Core.Frontend.Hotkey;
-
+using Tsinswreng.CsErr;
 [Doc(@$"註冊所有全局快捷鍵
 依平臺 各自實現此接口
 ")]
 public interface I_RegisterGlobalHotKeys{
-	public nil RegisterGlobalHotKeys();
+	/// 成功則內返null
+	public IAnswer<obj?> RegisterGlobalHotKeys();
 }
