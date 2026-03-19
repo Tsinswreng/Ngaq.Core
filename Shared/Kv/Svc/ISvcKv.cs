cc183fc3c54fd14b316e1081f79a0e2cd17214cd
Tsinswreng <Tsinswreng@gmail.com>
2026-03-19 22:08:40 +0800

    緟構SvcKv

--- Shared/Kv/Svc/ISvcKv.cs
+++ Shared/Kv/Svc/ISvcKv.cs
@@ -8,8 +8,12 @@ using Tsinswreng.CsSql;
 /// TODO  增接口㕥刪(軟/硬);
 [Doc(@$"#See[{nameof(PoKv)}]")]
 public partial interface ISvcKv{
-	public Task<IAsyncEnumerable<PoKv?>> BatGetByOwnerEtKey(
-		IAsyncEnumerable<(IdUser, obj)> Owner_Key, CT Ct
+	public Task<IAsyncEnumerable<PoKv?>> BatGetByOwnerEtKI64(
+		IAsyncEnumerable<(IdUser, i64)> Owner_Key, CT Ct
+	);
+	
+	public Task<IAsyncEnumerable<PoKv?>> BatGetByOwnerEtKStr(
+		IAsyncEnumerable<(IdUser, str)> Owner_Key, CT Ct
 	);
 	
 	public Task<nil> BatSet(IAsyncEnumerable<PoKv> Kvs, CT Ct);
