9ca7e010fead04e2ce1aac22054a2ecd2ec0d983
Tsinswreng <Tsinswreng@gmail.com>
2026-03-22 10:37:44 +0800

    推ISvcWordV2

--- Shared/Word/Svc/ISvcWordV2.cs
+++ Shared/Word/Svc/ISvcWordV2.cs
@@ -1,14 +1,46 @@
+/* 
+看
+Ai.typ
+再看Spec下
+Db.typ
+Entity.typ
+SvcDao.typ
+Test.typ
+
+然後看
+`
+E:\_code\CsNgaq\Ngaq.Core\Shared\Word\Svc\ISvcWordV2.cs
+E:\_code\CsNgaq\Ngaq.Local\Domains\Word\Svc\SvcWord.*.cs
+`
+
+我要搞 測試驅動開發
+你先給我寫測試用例 針對 ISvcWordV2。
+在這個文件夾裏面寫 E:\_code\CsNgaq\Ngaq.Test\proj\Ngaq.Local.Test\Domains\Word\SvcWord\
+ */
+
 using Ngaq.Core.Frontend.Kv;
 using Ngaq.Core.Shared.StudyPlan.Models.Po.PreFilter;
 using Ngaq.Core.Shared.StudyPlan.Models.PreFilter;
 using Ngaq.Core.Shared.User.UserCtx;
 using Ngaq.Core.Shared.Word.Models;
+using Ngaq.Core.Shared.Word.Models.Learn_;
+using Ngaq.Core.Shared.Word.Models.Po.Kv;
 using Ngaq.Core.Shared.Word.Models.Po.Learn;
 using Ngaq.Core.Shared.Word.Models.Po.Word;
 using Tsinswreng.CsSql;
 
 namespace Ngaq.Core.Shared.Word.Svc;
 
+[Doc(@$"
+Word Service Version 2. for {nameof(JnWord)}
+the old version is {nameof(ISvcWord)}, for it uses the pattern of FnXxx and returns inner function
+and did not support well for batch operations.
+
+一切涉及修改{nameof(JnWord)}的操作都要修改其 {nameof(PoWord.BizUpdatedAt)}
+```cs
+RepoWord.AsAppRepo().BatBizTouch()
+```
+")]
 public interface ISvcWordV2{
 	
 	[Doc(@$"
@@ -41,5 +73,17 @@ public interface ISvcWordV2{
 		,IAsyncEnumerable<PoWordLearn> PoWordLearnAsyE
 	);
 	
-
+	[Doc(@$"
+	從生詞表中加入單詞 用于學習。
+	來自生詞表的生單詞不應具有{nameof(JnWord.Learns)}、只有 {nameof(JnWord.Word)} 與 {nameof(JnWord.Props)}。
+	遇到({nameof(PoWord.Head)}, {nameof(PoWord.Lang)})相同的 即合併其{nameof(JnWord.Props)}
+	會新增{nameof(ELearn.Add)}、
+	按 新詞的總共的 新來的 {nameof(KeysProp.description)} 的數量
+	決定 新增的{nameof(ELearn.Add)}的數量
+	")]
+	public Task<nil> BatAddNewWordToLearn(
+		IDbFnCtx? Ctx, IUserCtx User,
+		IAsyncEnumerable<PoWord> PoWordAsyE
+	);
+	
 }
