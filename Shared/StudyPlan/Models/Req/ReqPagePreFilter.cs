4aed36b0ae779b799ba822c2342ee25ea6b84da8
Tsinswreng <Tsinswreng@gmail.com>
2026-03-24 09:55:50 +0800

    Dto不帶UserId

--- Shared/StudyPlan/Models/Req/ReqPagePreFilter.cs
+++ Shared/StudyPlan/Models/Req/ReqPagePreFilter.cs
@@ -3,7 +3,6 @@ using Tsinswreng.CsPage;
 
 namespace Ngaq.Core.Shared.StudyPlan.Models.Req;
 public class ReqPagePreFilter{
-	public IdUser Owner{get;set;}
 	public IPageQry PageQry{get;set;}
 	[Doc(@$"若空則不用此字段作篩選")]
 	public str? UniqNameSearch{get;set;} = null;
--- Shared/StudyPlan/Models/Req/ReqPageWeightArg.cs
+++ Shared/StudyPlan/Models/Req/ReqPageWeightArg.cs
@@ -3,7 +3,6 @@ using Tsinswreng.CsPage;
 
 namespace Ngaq.Core.Shared.StudyPlan.Models.Req;
 public class ReqPageWeightArg{
-	public IdUser Owner{get;set;}
 	public IPageQry PageQry{get;set;}
 	public str? UniqNameSearch{get;set;} = null;
 }
