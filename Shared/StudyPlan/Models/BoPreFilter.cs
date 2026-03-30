4d7b6f0b3dcfe0e7a752c4c3256642c75b820b9c
Tsinswreng <Tsinswreng@gmail.com>
2026-03-30 22:42:17 +0800

    ai:叶FromPoPreFilter

--- Shared/StudyPlan/Models/BoPreFilter.cs
+++ Shared/StudyPlan/Models/BoPreFilter.cs
@@ -4,13 +4,15 @@ using Ngaq.Core.Shared.StudyPlan.Models.PreFilter;
 namespace Ngaq.Core.Shared.StudyPlan.Models;
 
 public class BoPreFilter{
-	public PoPreFilter PoPreFilter { get; set; }
-	public PreFilter.PreFilter PreFilter{get;set;}
+	public PoPreFilter PoPreFilter { get; set; } = new();
+	public PreFilter.PreFilter PreFilter{get;set;} = new();
 	
 	[Doc(@$"原地賦值 要初始化 {nameof(PoPreFilter)} 和 {nameof(PreFilter)}.
 	#See[{nameof(ExtnPreFilter.FromPo)}]
 	")]
 	public void FromPoPreFilter(PoPreFilter PoPreFilter){
-		throw new NotImplementedException();
+		this.PoPreFilter = PoPreFilter;
+		this.PreFilter = new PreFilter.PreFilter();
+		this.PreFilter.FromPo(PoPreFilter);
 	}
 }
