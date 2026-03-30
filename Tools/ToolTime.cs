db74d36a5fe75adcbedb8e443ec9048c03dc336c
Tsinswreng <Tsinswreng@gmail.com>
2026-03-30 11:24:15 +0800

    治 ToolTime

---
+++ Tools/ToolTime.cs
@@ -0,0 +1,16 @@
+using Ngaq.Core.Infra;
+
+namespace Ngaq.Core.Tools;
+
+public static class ToolTime{
+	extension(Tempus z){
+	[Doc(@$"
+	用于簡單顯示 不佔太多空間
+	#Examples([26-03-30])
+	")]
+		public str YY_MM_DD(){
+			throw new NotImplementedException();
+		}
+	}
+	
+}
