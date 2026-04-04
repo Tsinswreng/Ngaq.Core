6a72bcef10c5483c7bfecac6ae54f4674708e63d
Tsinswreng <Tsinswreng@gmail.com>
2026-04-04 14:41:50 +0800

    从CsCfg之變

--- Infra/Cfg/ItemsClientCfg.cs
+++ Infra/Cfg/ItemsClientCfg.cs
@@ -2,50 +2,50 @@ namespace Ngaq.Core.Infra.Cfg;
 
 using System.Collections;
 using Tsinswreng.CsCfg;
-using static Tsinswreng.CsCfg.CfgItem<obj?>;
+using static Tsinswreng.CsCfg.CfgNode<obj?>;
 //TODO 異常處理 勿緣用戶配置ʹ謬而致整程序崩
 
 public class ItemsClientCfg{
-	public static ICfgItem<str> Lang = Mk(null, [nameof(Lang)], "default");
+	public static ICfgNode<str> Lang = Mk(null, [nameof(Lang)], "default");
 
 #if DEBUG&&false
 	public static ICfgItem<str> SqlitePath = Mk(null, [nameof(SqlitePath)], "./Ngaq.dev.sqlite");
 #else
 
-	public static ICfgItem<str> SqlitePath = Mk(null, [nameof(SqlitePath)], "./Ngaq.sqlite");
+	public static ICfgNode<str> SqlitePath = Mk(null, [nameof(SqlitePath)], "./Ngaq.sqlite");
 #endif
-	public static ICfgItem<str> RwCfgPath = Mk(null, [nameof(RwCfgPath)], "Ngaq.Rw.jsonc");
+	public static ICfgNode<str> RwCfgPath = Mk(null, [nameof(RwCfgPath)], "Ngaq.Rw.jsonc");
 
 	public class Background{
-		public static ICfgItem _R = Mk(null, [nameof(Background)]);
+		public static ICfgNode _R = Mk(null, [nameof(Background)]);
 		//[EnumOf("")]
-		public static ICfgItem<str> Mode = Mk(_R, [nameof(Mode)], "");
-		public static ICfgItem<IList<object?>> GalleryDirs = Mk(_R, [nameof(GalleryDirs)], (IList<object?>)[]);
+		public static ICfgNode<str> Mode = Mk(_R, [nameof(Mode)], "");
+		public static ICfgNode<IList<object?>> GalleryDirs = Mk(_R, [nameof(GalleryDirs)], (IList<object?>)[]);
 	}
 
 	// public static ICfgItem Background = Mk(null, [nameof(Background)], null);
 	// 	public static ICfgItem<IList<object?>> GalleryDirs = Mk(Background, [nameof(GalleryDirs)], (IList<object?>)[]);
-	public static ICfgItem<str> ServerBaseUrl = Mk(null, [nameof(ServerBaseUrl)], "");
-	public static ICfgItem Ui = Mk(null, [nameof(Ui)], null);
-		public static ICfgItem<f64> BaseFontSize = Mk(Ui, [nameof(BaseFontSize)], 16.0);
+	public static ICfgNode<str> ServerBaseUrl = Mk(null, [nameof(ServerBaseUrl)], "");
+	public static ICfgNode Ui = Mk(null, [nameof(Ui)], null);
+		public static ICfgNode<f64> BaseFontSize = Mk(Ui, [nameof(BaseFontSize)], 16.0);
 
-	public static ICfgItem User = Mk(null, [nameof(User)], null);
-		public static ICfgItem<str> CurrentUserId = Mk(User, [nameof(CurrentUserId)], "0");
+	public static ICfgNode User = Mk(null, [nameof(User)], null);
+		public static ICfgNode<str> CurrentUserId = Mk(User, [nameof(CurrentUserId)], "0");
 
 	public class Word{
-		public static ICfgItem _R = Mk(null, [nameof(Word)]);
-		public static ICfgItem<str> WordsPackExportPath = Mk(_R, [nameof(WordsPackExportPath)], "");
-		public static ICfgItem<str> WordsPackImportPath = Mk(_R, [nameof(WordsPackImportPath)], "");
-		public static ICfgItem<u64> MaxDisplayedWordCount = Mk(_R, [nameof(MaxDisplayedWordCount)], 50ul);
-		public static ICfgItem<IList<obj?>?> FilterLanguage = Mk(_R, [nameof(FilterLanguage)], (IList<obj?>)null);
-		public static ICfgItem<bool> EnableRandomBackground = Mk(_R, [nameof(EnableRandomBackground)], false);
+		public static ICfgNode _R = Mk(null, [nameof(Word)]);
+		public static ICfgNode<str> WordsPackExportPath = Mk(_R, [nameof(WordsPackExportPath)], "");
+		public static ICfgNode<str> WordsPackImportPath = Mk(_R, [nameof(WordsPackImportPath)], "");
+		public static ICfgNode<u64> MaxDisplayedWordCount = Mk(_R, [nameof(MaxDisplayedWordCount)], 50ul);
+		public static ICfgNode<IList<obj?>?> FilterLanguage = Mk(_R, [nameof(FilterLanguage)], (IList<obj?>)null);
+		public static ICfgNode<bool> EnableRandomBackground = Mk(_R, [nameof(EnableRandomBackground)], false);
 	}
 
 	public class LlmDictionary{
-		public static ICfgItem _R = Mk(null, [nameof(LlmDictionary)]);
-		public static ICfgItem<str> ApiUrl = Mk(_R, [nameof(ApiUrl)], "");
-		public static ICfgItem<str> ApiKey = Mk(_R, [nameof(ApiKey)], "");
-		public static ICfgItem<str> Model = Mk(_R, [nameof(Model)], "");
+		public static ICfgNode _R = Mk(null, [nameof(LlmDictionary)]);
+		public static ICfgNode<str> ApiUrl = Mk(_R, [nameof(ApiUrl)], "");
+		public static ICfgNode<str> ApiKey = Mk(_R, [nameof(ApiKey)], "");
+		public static ICfgNode<str> Model = Mk(_R, [nameof(Model)], "");
 	}
 }
 
--- Shared/StudyPlan/Models/PreFilter/FieldsFilter.cs
+++ Shared/StudyPlan/Models/PreFilter/FieldsFilter.cs
@@ -2,7 +2,9 @@ namespace Ngaq.Core.Shared.StudyPlan.Models.PreFilter;
 
 [Doc($@"字段集合對應的篩選條件集合")]
 public class FieldsFilter{
-	[Doc($@"要應用篩選的字段名列表")]
+	[Doc($@"要應用篩選的字段名列表。
+	當設置了多個值時 即爲所有元素都應用相同的 {nameof(Filters)}
+	")]
 	public IList<str> Fields{get;set;} = [];
 	[Doc($@"針對字段列表的條件列表")]
 	public IList<FilterItem> Filters{get;set;} = [];
