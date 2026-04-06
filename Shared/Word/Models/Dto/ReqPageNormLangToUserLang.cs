08f8f3ff75a4895fdc9ee4b748b3f451bc337cdf
Tsinswreng <Tsinswreng@gmail.com>
2026-04-06 12:10:43 +0800

    製ISvcNormLangToUserLang

---
+++ Shared/Word/Models/Dto/ReqPageNormLangToUserLang.cs
@@ -0,0 +1,15 @@
+﻿using Ngaq.Core.Shared.Word.Models.Po.NormLangToUserLang;
+using Tsinswreng.CsPage;
+
+namespace Ngaq.Core.Shared.Word.Models.Dto;
+
+[Doc(@$"
+{nameof(PoNormLangToUserLang)} 分頁查詢入參。
+")]
+public class ReqPageNormLangToUserLang{
+	public IPageQry PageQry { get; set; }
+	[Doc(@$"根據用戶自定義語言名稱做模糊查詢。
+	#See[{nameof(PoNormLangToUserLang.UserLang)}]
+	")]
+	public str? UserLang { get; set; } = null;
+}
---
+++ Shared/Word/Svc/ISvcNormLangToUserLang.cs
@@ -0,0 +1,42 @@
+﻿using Ngaq.Core.Infra;
+using Ngaq.Core.Shared.Dictionary.Models;
+using Ngaq.Core.Shared.Word.Models.Dto;
+using Ngaq.Core.Shared.Word.Models.Po.NormLangToUserLang;
+using Tsinswreng.CsPage;
+
+namespace Ngaq.Core.Shared.Word.Svc;
+
+[Doc($"{nameof(PoNormLangToUserLang)} 服務介面。")]
+public interface ISvcNormLangToUserLang{
+	[Doc("根據標準語言類型與標識值，查詢單個用戶自定義語言。")]
+	public Task<str?> GetUserLangByNormLang(
+		IDbUserCtx Ctx,
+		ELangIdentType NormLangType,
+		str NormLang,
+		CT Ct
+	);
+	
+	public Task<nil> BatAddNormLangToUserLang(
+		IDbUserCtx Ctx,
+		IAsyncEnumerable<PoNormLangToUserLang> Pos,
+		CT Ct
+	);
+	
+	public Task<nil> BatUpdNormLangToUserLang(
+		IDbUserCtx Ctx,
+		IAsyncEnumerable<PoNormLangToUserLang> Pos,
+		CT Ct
+	);
+	
+	public Task<nil> BatSoftDelNormLangToUserLang(
+		IDbUserCtx Ctx,
+		IAsyncEnumerable<PoNormLangToUserLang> Pos,
+		CT Ct
+	);
+	
+	public Task<IPageAsyE<PoNormLangToUserLang>> PageNormLangToUserLang(
+		IDbUserCtx Ctx,
+		ReqPageNormLangToUserLang Req,
+		CT Ct
+	);
+}
