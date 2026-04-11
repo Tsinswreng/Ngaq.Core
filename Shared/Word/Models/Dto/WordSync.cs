c260a5f7530b25580d652c381cc72bed13766162
Tsinswreng <Tsinswreng@gmail.com>
2026-04-11 17:02:18 +0800

    DtoJnWordSyncResult 繼承複用

--- Shared/Word/Models/Dto/WordSync.cs
+++ Shared/Word/Models/Dto/WordSync.cs
@@ -1,9 +1,7 @@
+using Ngaq.Core.Shared.Sync;
 using Ngaq.Core.Shared.Word.Models.Po.Word;
 
 namespace Ngaq.Core.Shared.Word.Models.Dto;
-
-
-
 
 
 
@@ -11,31 +9,6 @@ namespace Ngaq.Core.Shared.Word.Models.Dto;
 Remote合入Local。
 #See[{nameof(AggDiffCaseForSync)}]
 ")]
-public class DtoJnWordSyncResult{
-	public EDiffByBizIdResultForSync DiffResult{get;set;}
-	
-	[Doc(@$"適用于當{nameof(EDiffByBizIdResultForSync.RemoteIsNewer)}時。")]
-	public AggDiffCaseForSync? DiffCase{get;set;}
-	
-	[Doc(@$"同步前的Local 原詞")]
-	public JnWord? Local{get;set;}
-	[Doc(@$"Remote原詞")]
-	public JnWord? Remote{get;set;}
-	
-	[Doc(@$"僅含 Remote 比 Local多出的新資產。
-	不考慮其{nameof(JnWord.Word)}
-	")]
-	public JnWord? NewAssets{get;set;}
-	[Doc(@$"僅含 Remote 比 Local 更改的資產。
-	不考慮其{nameof(JnWord.Word)}
-	")]
-	public JnWord? ChangedAssets{get;set;}
-	
-	[Doc(@$"同步後的{nameof(JnWord.Word)}。
-	用來對應更新Local的PoWord。
-	")]
-	public PoWord? SyncedPoWord{get;set;}
+public class DtoJnWordSyncResult:AggSyncResult<JnWord>{
+
 }
-
-
-
