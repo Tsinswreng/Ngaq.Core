d598cee5758fdde7ac140a7eed073eafc2bc4971
Tsinswreng <Tsinswreng@gmail.com>
2026-04-11 17:00:57 +0800

    EDiffByBizIdResultForSync改成Class枚舉

--- Shared/Sync/EDiffByBizIdResultForSync.cs
+++ Shared/Sync/EDiffByBizIdResultForSync.cs
@@ -2,25 +2,27 @@ using Ngaq.Core.Shared.Word.Models;
 using Ngaq.Core.Shared.Word.Models.Po.Word;
 
 namespace Ngaq.Core.Shared.Sync;
+
+using E = EDiffByBizIdResultForSync;
 [Doc(@$"用于兩持久對象同步。
 按業務層唯一標識(BizId)比較、而不是按數據庫中生成的Id。
 ")]
-public enum EDiffByBizIdResultForSync{
+public class EDiffByBizIdResultForSync{
 	[Doc(@$"兩個對象都沒有變化。不用做。")]
-	NoChange,
+	public static readonly E NoChange = new();
 	
 	[Doc(@$"常見情況")]
-	RemoteIsNewer,
+	public static readonly E RemoteIsNewer = new();
 	
 	[Doc(@$"Remote更舊、不需要改動Local。
 	==true時 忽略其他字段的值 因爲已經不需操作了
 	")]
-	RemoteIsOlder,
+	public static readonly E RemoteIsOlder = new();
 	
 	[Doc(@$"Remote 在 接收合入的一方 沒有對應的Local。
 	直接把Remote入庫即可。
 	")]
-	LocalNotExist,
+	public static readonly E LocalNotExist = new();
 	
 	[Doc(@$"
 	Remote和Local的Id不一致、
@@ -35,5 +37,5 @@ public enum EDiffByBizIdResultForSync{
 	Local和Remote中{nameof(PoWord.BizCreatedAt)}最小者的PoWord
 	")]
 	
-	IdNotEqual,
+	public static readonly E IdNotEqual = new();
 }
