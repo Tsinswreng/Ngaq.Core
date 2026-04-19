ce68972607aba42d3464552304a2c2de96868fd6
Tsinswreng <Tsinswreng@gmail.com>
2026-04-19 11:22:40 +0800

    改 KeysUrl

---
+++ Infra/Url/KeysUrl.cs
@@ -0,0 +1,38 @@
+namespace Ngaq.Core.Infra.Url;
+
+using Ngaq.Core.Tools;
+using static Ngaq.Core.Tools.Url;
+
+
+[Doc(@$"http api url鍵定義。
+約定
+- `Open/`開頭的不用驗令牌;
+- `Api/`開頭的要驗令牌;
+")]
+public partial class KeysUrl{
+	public static Url OpenV1 = Mk(null, ["Open", "V1"]);
+	public static Url ApiV1 = Mk(null, ["Api", "V1"]);
+
+	//勿作深層內部類。䀬ʹ子ʹApi枚舉內部類只置于ConstUrlʹ直ʹ子層
+	public partial class OpenUser{
+		//public static Url _Root = "";
+		public static Url _R = Mk(OpenV1, ["Sys", "User"]);
+		public static Url Login = Mk(_R, [nameof(Login)]);
+		public static Url AddUser = Mk(_R, [nameof(AddUser)]);
+		public static Url TokenRefresh = Mk(_R, ["Token", "Refresh"]);
+	}
+
+	public partial class ApiUser{
+		public static Url _R = Mk(ApiV1, ["Sys", "User"]);
+		public static Url Logout = Mk(_R, [nameof(Logout)]);
+	}
+
+
+	public partial class Word{
+		public static Url _R = Mk(ApiV1, [nameof(Word)]);
+		public static Url Push = Mk(_R, [nameof(Push)]);
+		public static Url Pull = Mk(_R, [nameof(Pull)]);
+	}
+
+}
+
