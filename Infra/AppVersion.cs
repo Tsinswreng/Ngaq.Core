c68b7d0f0d1ad6efbd39147ff655256707a02e42
Tsinswreng <Tsinswreng@gmail.com>
2026-04-29 18:54:27 +0800

    AppVer

--- Infra/AppVersion.cs
+++ Infra/AppVersion.cs
@@ -1,5 +1,11 @@
 namespace Ngaq.Core.Infra;
 
+[Doc(@"
+new Version:(1, 2, 3, 261230102)
+版本: 1.2.3
+時間: 26年 第12週 週3 01:02
+應按 中時區。
+")]
 public class AppVer{
 	protected static AppVer? _Inst = null;
 	public static AppVer Inst => _Inst??= new AppVer();
